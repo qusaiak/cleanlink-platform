f2238147ff234afd3deec33138b152ba1a8fdfbe
qusai <qusai2000abokhier@gmail.com>
2026-04-27 15:46:33 +0300

    generated files

--- mobile/client_app/lib/core/utils/gen/assets.gen.dart
+++ mobile/client_app/lib/core/utils/gen/assets.gen.dart
@@ -18,68 +18,25 @@ import 'package:vector_graphics/vector_graphics.dart' as _vg;
 class $AssetsAnimationsGen {
   const $AssetsAnimationsGen();
 
-  /// File path: assets/animations/loading_icon.json
-  LottieGenImage get loadingIcon => const LottieGenImage('assets/animations/loading_icon.json');
-
   /// File path: assets/animations/splash_animation.json
   LottieGenImage get splashAnimation => const LottieGenImage('assets/animations/splash_animation.json');
 
   /// List of all assets
-  List<LottieGenImage> get values => [loadingIcon, splashAnimation];
-}
-
-class $AssetsCaGen {
-  const $AssetsCaGen();
-
-  /// File path: assets/ca/lets-encrypt-r3.pem
-  String get letsEncryptR3 => 'assets/ca/lets-encrypt-r3.pem';
-
-  /// List of all assets
-  List<String> get values => [letsEncryptR3];
+  List<LottieGenImage> get values => [splashAnimation];
 }
 
 class $AssetsIconsGen {
   const $AssetsIconsGen();
 
-  /// Directory path: assets/icons/home
-  $AssetsIconsHomeGen get home => const $AssetsIconsHomeGen();
-
-  /// File path: assets/icons/no_results_found.svg
-  SvgGenImage get noResultsFound => const SvgGenImage('assets/icons/no_results_found.svg');
-
-  /// File path: assets/icons/not-found.png
-  AssetGenImage get notFound => const AssetGenImage('assets/icons/not-found.png');
-
-  /// File path: assets/icons/notification.png
-  AssetGenImage get notification => const AssetGenImage('assets/icons/notification.png');
-
-  /// Directory path: assets/icons/player
-  $AssetsIconsPlayerGen get player => const $AssetsIconsPlayerGen();
-
-  /// File path: assets/icons/podcastsT.png
-  AssetGenImage get podcastsT => const AssetGenImage('assets/icons/podcastsT.png');
-
-  /// File path: assets/icons/songsT.png
-  AssetGenImage get songsT => const AssetGenImage('assets/icons/songsT.png');
-
-  /// File path: assets/icons/videosT.png
-  AssetGenImage get videosT => const AssetGenImage('assets/icons/videosT.png');
+  /// File path: assets/icons/syria.svg
+  SvgGenImage get syria => const SvgGenImage('assets/icons/syria.svg');
 
   /// List of all assets
-  List<dynamic> get values => [noResultsFound, notFound, notification, podcastsT, songsT, videosT];
+  List<SvgGenImage> get values => [syria];
 }
 
 class $AssetsImagesGen {
   const $AssetsImagesGen();
-
-  /// File path: assets/images/1.png
-  AssetGenImage get a1 => const AssetGenImage('assets/images/1.png');
-
-  /// File path: assets/images/2.png
-  AssetGenImage get a2 => const AssetGenImage('assets/images/2.png');
-
-  /// Directory path: assets/images/banner
-  $AssetsImagesBannerGen get banner => const $AssetsImagesBannerGen();
 
   /// Directory path: assets/images/logo
   $AssetsImagesLogoGen get logo => const $AssetsImagesLogoGen();
@@ -89,128 +46,32 @@ class $AssetsImagesGen {
 
   /// Directory path: assets/images/placeholders
   $AssetsImagesPlaceholdersGen get placeholders => const $AssetsImagesPlaceholdersGen();
-
-  /// Directory path: assets/images/test
-  $AssetsImagesTestGen get test => const $AssetsImagesTestGen();
-
-  /// List of all assets
-  List<AssetGenImage> get values => [a1, a2];
-}
-
-class $AssetsIconsHomeGen {
-  const $AssetsIconsHomeGen();
-
-  /// File path: assets/icons/home/_ic_album.xml
-  String get aIcAlbum => 'assets/icons/home/_ic_album.xml';
-
-  /// File path: assets/icons/home/ic_free_badge.png
-  AssetGenImage get icFreeBadge => const AssetGenImage('assets/icons/home/ic_free_badge.png');
-
-  /// File path: assets/icons/home/ic_outline_music_note.svg
-  SvgGenImage get icOutlineMusicNote => const SvgGenImage('assets/icons/home/ic_outline_music_note.svg');
-
-  /// File path: assets/icons/home/ic_video (1).svg
-  SvgGenImage get icVideo1 => const SvgGenImage('assets/icons/home/ic_video (1).svg');
-
-  /// File path: assets/icons/home/play_circle_outline.svg
-  SvgGenImage get playCircleOutline => const SvgGenImage('assets/icons/home/play_circle_outline.svg');
-
-  /// File path: assets/icons/home/unshuffle.png
-  AssetGenImage get unshuffle => const AssetGenImage('assets/icons/home/unshuffle.png');
-
-  /// List of all assets
-  List<dynamic> get values => [aIcAlbum, icFreeBadge, icOutlineMusicNote, icVideo1, playCircleOutline, unshuffle];
-}
-
-class $AssetsIconsPlayerGen {
-  const $AssetsIconsPlayerGen();
-
-  /// File path: assets/icons/player/Queue.png
-  AssetGenImage get queue => const AssetGenImage('assets/icons/player/Queue.png');
-
-  /// File path: assets/icons/player/fast_forward.png
-  AssetGenImage get fastForward => const AssetGenImage('assets/icons/player/fast_forward.png');
-
-  /// File path: assets/icons/player/no-repeat.png
-  AssetGenImage get noRepeat => const AssetGenImage('assets/icons/player/no-repeat.png');
-
-  /// File path: assets/icons/player/no-shuffle.png
-  AssetGenImage get noShuffle => const AssetGenImage('assets/icons/player/no-shuffle.png');
-
-  /// File path: assets/icons/player/repeat-one.png
-  AssetGenImage get repeatOne => const AssetGenImage('assets/icons/player/repeat-one.png');
-
-  /// File path: assets/icons/player/repeat.png
-  AssetGenImage get repeat => const AssetGenImage('assets/icons/player/repeat.png');
-
-  /// File path: assets/icons/player/rewind.png
-  AssetGenImage get rewind => const AssetGenImage('assets/icons/player/rewind.png');
-
-  /// File path: assets/icons/player/shuffle.png
-  AssetGenImage get shuffle => const AssetGenImage('assets/icons/player/shuffle.png');
-
-  /// List of all assets
-  List<AssetGenImage> get values => [queue, fastForward, noRepeat, noShuffle, repeatOne, repeat, rewind, shuffle];
-}
-
-class $AssetsImagesBannerGen {
-  const $AssetsImagesBannerGen();
-
-  /// File path: assets/images/banner/Upgrade-Arb.gif
-  AssetGenImage get upgradeArb => const AssetGenImage('assets/images/banner/Upgrade-Arb.gif');
-
-  /// File path: assets/images/banner/Upgrade-Eng.gif
-  AssetGenImage get upgradeEng => const AssetGenImage('assets/images/banner/Upgrade-Eng.gif');
-
-  /// List of all assets
-  List<AssetGenImage> get values => [upgradeArb, upgradeEng];
 }
 
 class $AssetsImagesLogoGen {
   const $AssetsImagesLogoGen();
 
-  /// File path: assets/images/logo/app_logo_disabled.png
-  AssetGenImage get appLogoDisabled => const AssetGenImage('assets/images/logo/app_logo_disabled.png');
-
-  /// File path: assets/images/logo/app_logo_enabled.png
-  AssetGenImage get appLogoEnabled => const AssetGenImage('assets/images/logo/app_logo_enabled.png');
-
-  /// File path: assets/images/logo/doremi-text.png
-  AssetGenImage get doremiText => const AssetGenImage('assets/images/logo/doremi-text.png');
-
-  /// File path: assets/images/logo/ic_app_logo.jpg
-  AssetGenImage get icAppLogoJpg => const AssetGenImage('assets/images/logo/ic_app_logo.jpg');
-
-  /// File path: assets/images/logo/ic_app_logo.png
-  AssetGenImage get icAppLogoPng => const AssetGenImage('assets/images/logo/ic_app_logo.png');
-
-  /// File path: assets/images/logo/ic_app_logo_foreground.png
-  AssetGenImage get icAppLogoForeground => const AssetGenImage('assets/images/logo/ic_app_logo_foreground.png');
-
-  /// File path: assets/images/logo/ic_app_logo_round.png
-  AssetGenImage get icAppLogoRound => const AssetGenImage('assets/images/logo/ic_app_logo_round.png');
+  /// File path: assets/images/logo/app_logo.jpg
+  AssetGenImage get appLogo => const AssetGenImage('assets/images/logo/app_logo.jpg');
 
   /// List of all assets
-  List<AssetGenImage> get values => [appLogoDisabled, appLogoEnabled, doremiText, icAppLogoJpg, icAppLogoPng, icAppLogoForeground, icAppLogoRound];
+  List<AssetGenImage> get values => [appLogo];
 }
 
 class $AssetsImagesOnboardingGen {
   const $AssetsImagesOnboardingGen();
 
-  /// File path: assets/images/onboarding/intro_image1.jpg
-  AssetGenImage get introImage1 => const AssetGenImage('assets/images/onboarding/intro_image1.jpg');
+  /// File path: assets/images/onboarding/intro_image1.png
+  AssetGenImage get introImage1 => const AssetGenImage('assets/images/onboarding/intro_image1.png');
 
-  /// File path: assets/images/onboarding/intro_image2.jpg
-  AssetGenImage get introImage2 => const AssetGenImage('assets/images/onboarding/intro_image2.jpg');
+  /// File path: assets/images/onboarding/intro_image2.png
+  AssetGenImage get introImage2 => const AssetGenImage('assets/images/onboarding/intro_image2.png');
 
-  /// File path: assets/images/onboarding/intro_image3.jpg
-  AssetGenImage get introImage3 => const AssetGenImage('assets/images/onboarding/intro_image3.jpg');
-
-  /// File path: assets/images/onboarding/intro_image4.jpg
-  AssetGenImage get introImage4 => const AssetGenImage('assets/images/onboarding/intro_image4.jpg');
+  /// File path: assets/images/onboarding/intro_image3.png
+  AssetGenImage get introImage3 => const AssetGenImage('assets/images/onboarding/intro_image3.png');
 
   /// List of all assets
-  List<AssetGenImage> get values => [introImage1, introImage2, introImage3, introImage4];
+  List<AssetGenImage> get values => [introImage1, introImage2, introImage3];
 }
 
 class $AssetsImagesPlaceholdersGen {
@@ -219,61 +80,19 @@ class $AssetsImagesPlaceholdersGen {
   /// File path: assets/images/placeholders/image_placeholder.jpg
   AssetGenImage get imagePlaceholder => const AssetGenImage('assets/images/placeholders/image_placeholder.jpg');
 
-  /// File path: assets/images/placeholders/image_placeholder_dark.png
-  AssetGenImage get imagePlaceholderDark => const AssetGenImage('assets/images/placeholders/image_placeholder_dark.png');
-
-  /// File path: assets/images/placeholders/img_album_default.png
-  AssetGenImage get imgAlbumDefault => const AssetGenImage('assets/images/placeholders/img_album_default.png');
-
-  /// File path: assets/images/placeholders/img_album_default_dark.png
-  AssetGenImage get imgAlbumDefaultDark => const AssetGenImage('assets/images/placeholders/img_album_default_dark.png');
-
-  /// File path: assets/images/placeholders/img_podcast_default.png
-  AssetGenImage get imgPodcastDefault => const AssetGenImage('assets/images/placeholders/img_podcast_default.png');
-
-  /// File path: assets/images/placeholders/img_podcast_default_dark.png
-  AssetGenImage get imgPodcastDefaultDark => const AssetGenImage('assets/images/placeholders/img_podcast_default_dark.png');
-
-  /// File path: assets/images/placeholders/img_song_default.png
-  AssetGenImage get imgSongDefault => const AssetGenImage('assets/images/placeholders/img_song_default.png');
-
-  /// File path: assets/images/placeholders/img_song_default_dark.png
-  AssetGenImage get imgSongDefaultDark => const AssetGenImage('assets/images/placeholders/img_song_default_dark.png');
-
-  /// File path: assets/images/placeholders/img_video_default.png
-  AssetGenImage get imgVideoDefault => const AssetGenImage('assets/images/placeholders/img_video_default.png');
-
-  /// File path: assets/images/placeholders/img_video_default_dark.png
-  AssetGenImage get imgVideoDefaultDark => const AssetGenImage('assets/images/placeholders/img_video_default_dark.png');
-
   /// File path: assets/images/placeholders/person_placeholder.jpg
   AssetGenImage get personPlaceholder => const AssetGenImage('assets/images/placeholders/person_placeholder.jpg');
 
   /// List of all assets
-  List<AssetGenImage> get values => [imagePlaceholder, imagePlaceholderDark, imgAlbumDefault, imgAlbumDefaultDark, imgPodcastDefault, imgPodcastDefaultDark, imgSongDefault, imgSongDefaultDark, imgVideoDefault, imgVideoDefaultDark, personPlaceholder];
-}
-
-class $AssetsImagesTestGen {
-  const $AssetsImagesTestGen();
-
-  /// File path: assets/images/test/mm.jpg
-  AssetGenImage get mm => const AssetGenImage('assets/images/test/mm.jpg');
-
-  /// List of all assets
-  List<AssetGenImage> get values => [mm];
+  List<AssetGenImage> get values => [imagePlaceholder, personPlaceholder];
 }
 
 class Assets {
   const Assets._();
 
   static const $AssetsAnimationsGen animations = $AssetsAnimationsGen();
-  static const $AssetsCaGen ca = $AssetsCaGen();
   static const $AssetsIconsGen icons = $AssetsIconsGen();
   static const $AssetsImagesGen images = $AssetsImagesGen();
-  static const String localConfig = 'local_config.env';
-
-  /// List of all assets
-  static List<String> get values => [localConfig];
 }
 
 class AssetGenImage {
