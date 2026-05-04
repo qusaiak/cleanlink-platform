aeda4c8ad51d6e5d670c01201dcc4a362ceae596
qusai <qusai2000abokhier@gmail.com>
2026-05-04 12:27:20 +0300

    edit theme and language config

--- mobile/client_app/lib/app.dart
+++ mobile/client_app/lib/app.dart
@@ -2,11 +2,14 @@ import 'package:client_app/config/routes/app_router.dart';
 import 'package:client_app/config/theme/app_themes.dart';
 import 'package:client_app/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:client_app/features/base/presentation/bloc/base_bloc.dart';
+import 'package:client_app/features/profile/presentation/bloc/profile_bloc.dart';
 import 'package:client_app/injection_container.dart';
 import 'package:client_app/l10n/app_localizations.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'config/theme/app_theme_info.dart';
+import 'l10n/l10n.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -17,21 +20,28 @@ class MyApp extends StatelessWidget {
       providers: [
         BlocProvider<AuthBloc>(create: (BuildContext context) => sl()),
         BlocProvider<BaseBloc>(create: (BuildContext context) => sl()),
+        BlocProvider<ProfileBloc>(create: (BuildContext context) => sl()),
       ],
       child: ScreenUtilInit(
-        designSize: const Size(375, 812),
         minTextAdapt: true,
+        splitScreenMode: true,
         builder: (_, child) {
-          return MaterialApp.router(
-            debugShowCheckedModeBanner: false,
-            title: 'CleanLink',
-            theme: lightTheme(),
-            darkTheme: darkTheme(),
-            themeMode: ThemeMode.light,
-            localizationsDelegates: AppLocalizations.localizationsDelegates,
-            supportedLocales: AppLocalizations.supportedLocales,
-            locale: const Locale('en'),
-            routerConfig: AppRouter.router,
+          return BlocBuilder<ProfileBloc, ProfileState>(
+            builder: (context, state) {
+              return MaterialApp.router(
+                debugShowCheckedModeBanner: false,
+                title: 'CleanLink',
+                theme: lightTheme(),
+                darkTheme: darkTheme(),
+                themeMode: AppThemeInfo.isLight
+                    ? ThemeMode.light
+                    : ThemeMode.dark,
+                locale: Locale(state.languageCode),
+                supportedLocales: L10n.all,
+                localizationsDelegates: AppLocalizations.localizationsDelegates,
+                routerConfig: AppRouter.router,
+              );
+            },
           );
         },
       ),
--- mobile/client_app/lib/injection_container.dart
+++ mobile/client_app/lib/injection_container.dart
@@ -1,5 +1,6 @@
 import 'package:client_app/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:client_app/features/base/presentation/bloc/base_bloc.dart';
+import 'package:client_app/features/profile/presentation/bloc/profile_bloc.dart';
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 import 'package:internet_connection_checker/internet_connection_checker.dart';
@@ -9,6 +10,7 @@ final sl = GetIt.instance;
 Future<void> initializeDependencies() async {
   sl.registerFactory<AuthBloc>(() => AuthBloc());
   sl.registerFactory<BaseBloc>(() => BaseBloc());
+  sl.registerFactory<ProfileBloc>(() => ProfileBloc());
 
   /// =========================
   /// EXTERNAL
